5dac0cd01a79484ebd29eee4e6d30dcf59e199d4
Nicola Iarocci <nicola@nicolaiarocci.com>
2017-07-03 15:58:02 +0200

    Use heroku host and port when available

--- run.py
+++ run.py
@@ -1,11 +1,20 @@
+import os
+
 from flask import Flask
 from flask_sentinel import ResourceOwnerPasswordCredentials
 
-# still a WIP
+port = os.environ.get('PORT')
+if port:
+    # Heroku
+    host = '0.0.0.0'
+    port = int(port)
+else:
+    host = '127.0.0.1'
+    port = 8000
 
 app = Flask(__name__)
 app.config.from_object('settings')
 
 if __name__ == '__main__':
     ResourceOwnerPasswordCredentials(app)
-    app.run(ssl_context='adhoc', port=8000)
+    app.run(ssl_context='adhoc', host=host, port=port)
