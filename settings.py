220a185f0a6c3b1c0956ec4f7f267e5c1eb0fbf4
Nicola Iarocci <nicola@nicolaiarocci.com>
2017-07-03 16:07:55 +0200

    Add mongo login settings

--- settings.py
+++ settings.py
@@ -1,7 +1,13 @@
 import os
+import urllib
 
-SENTINEL_MONGO_DBNAME = 'adam-oauth'
 DEBUG = True
+
+user = os.environ.get('MONGO_USERNAME', 'user')
+pw = urllib.parse.quote_plus(os.environ.get('MONGO_PASSWORD', 'pw'))
+uri = os.environ.get('MONGO_URI', 'localhost')
+SENTINEL_MONGO_URI = "mongodb://%s:%s%s" % (user, pw, uri)
+SENTINEL_MONGO_DBNAME = 'adam-oauth'
 
 redis_url = os.environ.get('REDIS_URL')
 if redis_url:
