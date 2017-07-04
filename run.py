10b0f8ea5a2b772d635f65bf6e85f3916f70babe
Nicola Iarocci <nicola@nicolaiarocci.com>
2017-07-04 09:59:13 +0200

    On Heroku don't use ssl_context as it causes H13 error

--- run.py
+++ run.py
@@ -5,16 +5,17 @@ from flask_sentinel import ResourceOwnerPasswordCredentials
 
 port = os.environ.get('PORT')
 if port:
-    # Heroku
     host = '0.0.0.0'
     port = int(port)
+    context = None  # ssl provided by the heroku host
 else:
     host = '127.0.0.1'
     port = 8000
+    context = 'adhoc'
 
 app = Flask(__name__)
 app.config.from_object('settings')
 
 if __name__ == '__main__':
     ResourceOwnerPasswordCredentials(app)
-    app.run(ssl_context='adhoc', host=host, port=port)
+    app.run(host=host, port=port, ssl_context=context)
